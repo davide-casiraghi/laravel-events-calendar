7e2cdb073e4bc784800465704c0b0172119939a8
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-01 07:51:29 +0200

    added phpunit

--- src/LaravelEventsCalendar.php
+++ src/LaravelEventsCalendar.php
@@ -4,5 +4,48 @@ namespace DavideCasiraghi\LaravelEventsCalendar;
 
 class LaravelEventsCalendar
 {
-    // Build your next great package.
+    /***************************************************************************/
+
+    /**
+     * Format the start date to be used in the search query.
+     * If the start date is null return today's date.
+     *
+     * @param  int  event id
+     * @return \App\Event the active events collection
+     */
+    public static function prepareStartDate($DatePickerStartDate)
+    {
+        if ($DatePickerStartDate) {
+            list($tid, $tim, $tiy) = explode('/', $DatePickerStartDate);
+            $ret = "$tiy-$tim-$tid";
+        } else {
+            // If no start date selected the search start from today's date
+            date_default_timezone_set('Europe/Rome');
+            $ret = date('Y-m-d', time());
+        }
+
+        return $ret;
+    }
+    
+    /***************************************************************************/
+
+    /**
+     * Format the edn date to be used in the search query.
+     *
+     * @param  int  event id
+     * @return \App\Event the active events collection
+     */
+    public static function prepareEndDate($DatePickerEndDate)
+    {
+        if ($DatePickerEndDate) {
+            list($tid, $tim, $tiy) = explode('/', $DatePickerEndDate);
+            $ret = "$tiy-$tim-$tid";
+        } else {
+            $ret = null;
+        }
+
+        return $ret;
+    }
+
+
 }
