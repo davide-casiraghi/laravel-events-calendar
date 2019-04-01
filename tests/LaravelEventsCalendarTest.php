cc02698669eb2237329b82a31da0bc1347d50b6c
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-01 06:43:50 +0000

    Apply fixes from StyleCI

--- tests/LaravelEventsCalendarTest.php
+++ tests/LaravelEventsCalendarTest.php
@@ -2,24 +2,22 @@
 
 namespace DavideCasiraghi\LaravelEventsCalendar\Tests;
 
-use DavideCasiraghi\LaravelEventsCalendar\LaravelEventsCalendar;
-use PHPUnit\Framework\TestCase;
 use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
+use DavideCasiraghi\LaravelEventsCalendar\LaravelEventsCalendar;
 
 class LaravelEventsCalendarTest extends TestCase
 {
     /** @test */
     public function it_prepare_start_date()
-    {    
+    {
         $todaysMysqlDateFormat = Carbon::now()->format('Y-m-d');
-        
+
         $startDateMysqlDateFormat = LaravelEventsCalendar::formatDatePickerDateForMysql(null);
         $this->assertEquals($startDateMysqlDateFormat, $todaysMysqlDateFormat);
-        
+
         $startDateFromDatePicker = Carbon::now()->format('d/m/Y');
         $startDateMysqlDateFormat = LaravelEventsCalendar::formatDatePickerDateForMysql($startDateFromDatePicker);
         $this->assertEquals($startDateMysqlDateFormat, $todaysMysqlDateFormat);
     }
-    
-    
 }
