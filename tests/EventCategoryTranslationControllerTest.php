534f2ac6a32e9f8a834180a17a4cdd79c9949479
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-04 22:51:56 +0200

    deleting file before upload test

--- tests/EventCategoryTranslationControllerTest.php
+++ tests/EventCategoryTranslationControllerTest.php
@@ -107,29 +107,53 @@ class EventCategoryTranslationControllerTest extends TestCase
 
         $this->post('/eventCategoryTranslations/store', $data);
 
+        // Update the translation
         $attributes = ([
             'event_category_translation_id' => 2,
             'language_code' => 'es',
             'name' => 'Spanish category name updated',
           ]);
-
         $response = $this->followingRedirects()
                          ->put('/eventCategoryTranslations/update', $attributes);
         $response->assertViewIs('laravel-events-calendar::eventCategories.index')
                  ->assertStatus(200);
         $this->assertDatabaseHas('event_category_translations', ['locale' => 'es', 'name' => 'Spanish category name updated']);
+    
+        // Update with no attributes - to not pass validation 
+        //$response = $this->followingRedirects()
+                        // ->put('/eventCategoryTranslations/update', [])->dump();
+                        // ->assertSessionHasErrors();
     }
 
     /** @test */
-    /*public function it_does_not_update_invalid_event_category()
+    public function it_does_not_update_invalid_event_category()
     {
         $user = User::first();
         auth()->login($user);
 
-        $eventCategory = factory(EventCategory::class)->create();
-        $response = $this->put('/eventCategories/'.$eventCategory->id, []);
+        $eventCategory = factory(EventCategory::class)->create([
+                            'name' => 'Regular Jams',
+                            'slug' => 'regular-jams',
+                        ]);
+
+        $data = [
+            'event_category_id' => $eventCategory->id,
+            'language_code' => 'es',
+            'name' => 'Spanish category name',
+        ];
+
+        $this->post('/eventCategoryTranslations/store', $data);
+
+        // Update the translation
+        $attributes = ([
+            'event_category_translation_id' => 2,
+            'language_code' => 'es',
+            'name' => '',
+          ]);
+        $response = $this->followingRedirects()
+                         ->put('/eventCategoryTranslations/update', $attributes);
         $response->assertSessionHasErrors();
-    }*/
+    }
 
     /** @test */
     public function it_deletes_event_category_translation()
