469765ac98e05cbedc5ddc0dfc4e38701e15deea
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-06 13:16:07 +0200

    authenticateAsAdmin now works for continents

--- tests/ContinentControllerTest.php
+++ tests/ContinentControllerTest.php
@@ -15,9 +15,7 @@ class ContinentControllerTest extends TestCase
     /** @test */
     public function it_displays_the_continents_index_page()
     {
-        // Authenticate the admin
         $this->authenticateAsAdmin();
-
         $this->get('continents')
             ->assertViewIs('laravel-events-calendar::continents.index')
             ->assertStatus(200);
@@ -26,6 +24,7 @@ class ContinentControllerTest extends TestCase
     /** @test */
     public function it_displays_the_continent_create_page()
     {
+        $this->authenticateAsAdmin();
         $this->get('continents/create')
             ->assertViewIs('laravel-events-calendar::continents.create')
             ->assertStatus(200);
@@ -34,11 +33,9 @@ class ContinentControllerTest extends TestCase
     /** @test */
     public function it_stores_a_valid_continent()
     {
+        $this->authenticateAsAdmin();
+        
         $attributes = factory(Continent::class)->raw();
-
-        $user = User::first();
-        auth()->login($user);
-
         $response = $this->post('/continents', $attributes);
         $continent = Continent::first();
 
@@ -49,6 +46,8 @@ class ContinentControllerTest extends TestCase
     /** @test */
     public function it_does_not_store_invalid_continent()
     {
+        $this->authenticateAsAdmin();
+        
         $response = $this->post('/continents', []);
         $response->assertSessionHasErrors();
         $this->assertNull(Continent::first());
@@ -57,6 +56,8 @@ class ContinentControllerTest extends TestCase
     /** @test */
     public function it_displays_the_continent_show_page()
     {
+        $this->authenticateAsAdmin();
+        
         $continent = factory(Continent::class)->create();
         $response = $this->get("/continents/{$continent->id}");
         $response->assertViewIs('laravel-events-calendar::continents.show')
@@ -66,6 +67,8 @@ class ContinentControllerTest extends TestCase
     /** @test */
     public function it_displays_the_continent_edit_page()
     {
+        $this->authenticateAsAdmin();
+        
         $continent = factory(Continent::class)->create();
         $response = $this->get("/continents/{$continent->id}/edit");
         $response->assertViewIs('laravel-events-calendar::continents.edit')
@@ -76,11 +79,10 @@ class ContinentControllerTest extends TestCase
     public function it_updates_valid_continent()
     {
         // https://www.neontsunami.com/posts/scaffolding-laravel-tests
+        $this->authenticateAsAdmin();
+        
         $continent = factory(Continent::class)->create();
         $attributes = factory(Continent::class)->raw(['name' => 'Updated']);
-
-        $user = User::first();
-        auth()->login($user);
 
         $response = $this->put("/continents/{$continent->id}", $attributes);
         $response->assertRedirect('/continents/');
@@ -90,6 +92,8 @@ class ContinentControllerTest extends TestCase
     /** @test */
     public function it_does_not_update_invalid_continent()
     {
+        $this->authenticateAsAdmin();
+        
         $continent = factory(Continent::class)->create(['name' => 'Example']);
         $response = $this->put("/continents/{$continent->id}", []);
         $response->assertSessionHasErrors();
@@ -99,6 +103,8 @@ class ContinentControllerTest extends TestCase
     /** @test */
     public function it_deletes_continents()
     {
+        $this->authenticateAsAdmin();
+        
         $continent = factory(Continent::class)->create();
         $response = $this->delete("/continents/{$continent->id}");
         $response->assertRedirect('/continents');
