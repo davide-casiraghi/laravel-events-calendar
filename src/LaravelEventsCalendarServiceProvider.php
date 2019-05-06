19e3b8a062757e5c8bf10f09a943a04cb58c5f82
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-06 17:38:21 +0200

    Apply fixes from StyleCI (#51)

--- src/LaravelEventsCalendarServiceProvider.php
+++ src/LaravelEventsCalendarServiceProvider.php
@@ -34,7 +34,7 @@ class LaravelEventsCalendarServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-events-calendar'),
             ], 'views');
-            
+
             $this->publishes([
                 __DIR__.'/../resources/assets/js' => resource_path('js/vendor/laravel-events-calendar/'),
             ], 'js');
