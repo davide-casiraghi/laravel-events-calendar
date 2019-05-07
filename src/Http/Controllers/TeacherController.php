93cb79bcca6765dd3d37a12a67ebdad4b473ef42
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-07 09:38:29 +0200

    trying to solve auth user instance of scrutinizer issue

--- src/Http/Controllers/TeacherController.php
+++ src/Http/Controllers/TeacherController.php
@@ -262,7 +262,8 @@ class TeacherController extends Controller
         $teacher->website = $request->get('website');
         $teacher->facebook = $request->get('facebook');
 
-        $teacher->created_by = \Auth::user()->id;
+        //$teacher->created_by = \Auth::user()->id;
+        $teacher->created_by = Auth::id();
 
         if (! $teacher->slug) {
             $teacher->slug = Str::slug($teacher->name, '-').'-'.rand(10000, 100000);
