fde931b827f22907720a9031b491b9f479d21449
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-19 22:07:06 +0200

    added some italian translations

---
+++ resources/lang/it/general.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'search' => 'Cerca',
+    'reset' => 'Resetta',
+    'edit' => 'Modifica',
+    'delete' => 'Cancella',
+    'code' => 'Codice',
+    'continent' => 'Continente',
+    'country' => 'Stato',
+    'name' => 'Nome',
+    'title' => 'Titolo',
+    'created_by' => 'Creato da',
+    'phone' => 'Telefono',
+    'website' => 'Sito web',
+    'description' => 'Descrizione',
+    'add_new_translation' => 'Aggiungi una nuova traduzione',
+    'delete_translation' => 'Elimina traduzione',
+    'select_owner' => 'Seleziona proprietario',
+    'select_country' => 'Seleziona una nazione',
+    'select_date' =>  'Scegliere una data',
+    'email_address' => 'Email',
+    'teachers' => 'Insegnanti',
+    'organizers' => 'Organizzatori',
+    'venues' => 'Luoghi',
+    'choose' => 'Scegliere...',
+    'select_one' => 'Seleziona uno',
+];
