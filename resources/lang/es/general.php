21557941082a7d0c422b9b6f855e3f6878be710c
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-20 15:28:02 +0200

    added spanish

---
+++ resources/lang/es/general.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'search' => 'Buscar',
+    'reset' => 'Deshacer',
+    'edit' => 'Editar',
+    'delete' => 'Borrar',
+    'code' => 'Código',
+    'continent' => 'Continente',
+    'country' => 'País',
+    'name' => 'Nombre',
+    'title' => 'Título',
+    'created_by' => 'Creado por',
+    'phone' => 'Teléfono',
+    'website' => 'Sitio web',
+    'description' => 'Descripción',
+    'add_new_translation' => 'Agregar una nueva traducción',
+    'delete_translation' => 'Eliminar traducción',
+    'select_owner' => 'Selecciona propietario/a',
+    'select_country' => 'Seleciona el país',
+    'select_date' => 'Selecciona la fecha',
+    'email_address' => 'Dirección de mail',
+    'teachers' => 'Profesor@s',
+    'organizers' => 'Organizador@s',
+    'venues' => 'Lugar',
+    'choose' => 'Escoger...',
+    'select_one' => 'Seleccione uno',
+];
