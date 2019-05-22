3df8d91692dea18806f6e2e43659bf51199d21c7
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-22 08:58:37 +0200

    added directives for the dates

--- src/LaravelEventsCalendarServiceProvider.php
+++ src/LaravelEventsCalendarServiceProvider.php
@@ -4,6 +4,7 @@ namespace DavideCasiraghi\LaravelEventsCalendar;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class LaravelEventsCalendarServiceProvider extends ServiceProvider
 {
@@ -96,6 +97,26 @@ class LaravelEventsCalendarServiceProvider extends ServiceProvider
                 __DIR__.'/../database/seeds/EventCategoriesTableSeeder.php' => database_path('seeds/EventCategoriesTableSeeder.php'),
             ], 'seed-event-categories');
         }
+        
+        /* Directives to manage the dates*/
+        Blade::directive('date', function ($expression) {
+            return "<?php echo date('d/m/Y', strtotime($expression))?>";
+        });
+        Blade::directive('date_monthname', function ($expression) {
+            /*return "<?php echo date('d M Y', strtotime($expression))?>";*/
+            return "<?php echo Carbon\Carbon::parse($expression)->isoFormat('D MMM YYYY'); ?>";
+        });
+        Blade::directive('day', function ($expression) {
+            return "<?php echo date('d', strtotime($expression))?>";
+        });
+        Blade::directive('month', function ($expression) {
+            /*return "<?php echo date('M', strtotime($expression))?>";*/
+            return "<?php echo Carbon\Carbon::parse($expression)->isoFormat('MMM')?>";
+        });
+        Blade::directive('time_am_pm', function ($expression) {
+            return "<?php echo date('g.i a', strtotime($expression))?>";
+        });
+
     }
 
     /**
