cc7997305ad8fd0bfafa109da4f308e45b3d0628
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-06-19 12:52:37 +0000

    Apply fixes from StyleCI

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -32,7 +32,7 @@ class Country extends Model
     {
         $minutes = 15;
         $ret = Cache::remember('countries_list', $minutes, function () {
-            return Country::orderBy('name')->pluck('name', 'id');
+            return self::orderBy('name')->pluck('name', 'id');
         });
 
         return $ret;
