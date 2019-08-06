91b67d48ed6c7ffd19b84afbf5ff06eed7b7cf73
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-08-06 14:17:05 +0200

    fixed problem that trigger form submit in vue.js teacher componetn

--- resources/assets/js/components/SelectTeacher.vue
+++ resources/assets/js/components/SelectTeacher.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            {{demo}}
             <br />
-            <button type="button" v-on:click="showModal">Show Modal</button>
+            <button type="button" v-on:click="showModal">Create a new teacher</button>
             
             <b-modal ref="myModal">
                 <div class="d-block text-center">
                     <h3>Fancy</h3>
+                    {{demo}}
                     <a class="fancybox" href="https://images.pexels.com/photos/1411677/pexels-photo-1411677.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
                         Open
                     </a>
