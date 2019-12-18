4ca07a371e2d484036a1cd6bfc86f7f9d6f4d697
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-12-18 09:24:38 +0100

    added translations for regions

---
+++ resources/lang/pt/region.php
@@ -0,0 +1,11 @@
+
+<?php
+
+return [
+    'add_new_region' => 'Adicionar novo Estado/Região',
+    'create_new_region' => 'Criar novo Estado/Região',
+    'edit_region' => 'Editar Estado/região',
+    'regions_management' => 'Gerenciamento de State/Regions',
+    'search_by_region_name' => 'Procurar por nome de Estado/Região',
+    'timezone' => 'Timezone',
+];
