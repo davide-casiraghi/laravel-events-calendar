92b3e35014e76775b6de1d3ed4d5cf17be549f36
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-03-04 18:53:39 +0100

    Fix retrieve-all-gps-coordinates command

--- src/Console/RetrieveAllGpsCoordinates.php
+++ src/Console/RetrieveAllGpsCoordinates.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace DavideCasiraghi\LaravelEventsCalendar\Console;
 
-use Illuminate\Console\Command;
 use DavideCasiraghi\LaravelEventsCalendar\Facades\LaravelEventsCalendar;
 use DavideCasiraghi\LaravelEventsCalendar\Models\Country;
 use DavideCasiraghi\LaravelEventsCalendar\Models\EventVenue;
+use Illuminate\Console\Command;
 
 class RetrieveAllGpsCoordinates extends Command
 {
@@ -13,37 +14,47 @@ class RetrieveAllGpsCoordinates extends Command
      *
      * @var string
      */
-    protected $signature =  'retrieve-all-gps-coordinates';
-    
+    protected $signature = 'retrieve-all-gps-coordinates';
+
     /**
      * The console command description.
      *
      * @var string
      */
     protected $description = 'Assign to all the venues the corresponding GPS coordinates';
-    
+
     /**
-    * Create a new command instance.
-    *
-    * @return void
-    */
-   public function __construct()
-   {
-       parent::__construct();
-   }
-   
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     /**
-    * Execute the console command.
-    *
-    * @param  
-    * @return mixed
-    */
-    public function handle(){
+     * Execute the console command.
+     *
+     * @param
+     * @return mixed
+     */
+    public function handle()
+    {
         $eventVenues = EventVenue::all();
+        $eventVenuesNumber = count($eventVenues);
+
         foreach ($eventVenues as $key => $eventVenue) {
+            
             // Get GPS coordinates
             $address = Country::getCountryName($eventVenue->country_id).', '.$eventVenue->city.', '.$eventVenue->address;
             $gpsCoordinates = LaravelEventsCalendar::getVenueGpsCoordinates($address);
+
+            // Print info on screen
+            $this->info($key." of ".$eventVenuesNumber." - ".$address);
+            $this->info($gpsCoordinates['lat']." ".$gpsCoordinates['lng']);
+            
+            // Save the data
             $eventVenue->lat = $gpsCoordinates['lat'];
             $eventVenue->lng = $gpsCoordinates['lng'];
             $eventVenue->save();
