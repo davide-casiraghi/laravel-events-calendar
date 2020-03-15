c8a3cd8d149e747d1f13acfd4e909b80c07d5d8c
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-03-15 08:16:01 +0000

    Apply fixes from StyleCI

--- tests/LaravelEventsCalendarTest.php
+++ tests/LaravelEventsCalendarTest.php
@@ -203,19 +203,17 @@ class LaravelEventsCalendarTest extends TestCase
     {
         // To test manually
         //https://developer.mapquest.com/documentation/tools/latitude-longitude-finder/
-        
+
         $address = 'Italy, Milano, via Dante, 15';
         $venuesCoordinates = LaravelEventsCalendar::getVenueGpsCoordinates($address);
         $this->assertSame($venuesCoordinates['lat'], 45.467293);
         $this->assertSame($venuesCoordinates['lng'], 9.183276);
-        
-        
+
         // https://www.mapquestapi.com/geocoding/v1/address?key=Ad5KVnAISxX6aHyj6fAnHcKeh30n4W60&location=Germany,%20Hasenheide,%2054+Berlin
         $address = 'Germany, Berlin, Hasenheide, 54';
         $venuesCoordinates = LaravelEventsCalendar::getVenueGpsCoordinates($address);
         $this->assertSame($venuesCoordinates['lat'], 52.489991);
         $this->assertSame($venuesCoordinates['lng'], 13.410147);
-        
     }
 
     /** @test */
@@ -257,8 +255,8 @@ class LaravelEventsCalendarTest extends TestCase
     {
         $cleanedString = LaravelEventsCalendar::cleanString('Köln');
         $this->assertSame($cleanedString, 'Koln');
-        
-        $cleanedString = LaravelEventsCalendar::cleanString("Højbjerg");
+
+        $cleanedString = LaravelEventsCalendar::cleanString('Højbjerg');
         $this->assertSame($cleanedString, 'Hojbjerg');
     }
 }
