04087cae1c3b0191b9c823269e34fca32d013230
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-03-31 16:52:20 +0200

    Apply fixes from StyleCI (#177)

--- tests/TeacherModelTest.php
+++ tests/TeacherModelTest.php
@@ -2,35 +2,33 @@
 
 namespace DavideCasiraghi\LaravelEventsCalendar\Tests;
 
-use Carbon\Carbon;
 use DavideCasiraghi\LaravelEventsCalendar\Models\Teacher;
 use Illuminate\Foundation\Testing\WithFaker;
 
-class EventModelTest extends TestCase
+class TeacherModelTest extends TestCase
 {
     use WithFaker;
 
     /***************************************************************/
-    
+
     /** @test */
     public function it_gets_the_teacher_creator()
     {
         $this->authenticateAsAdmin();
-        
+
         // Create a teacher by the administrator, that has id 1
         $attributes = factory(Teacher::class)->raw([
             'Name'=>'test teacher',
         ]);
         $this->post('/teachers', $attributes);
-        
+
         $teacher = Teacher::find(1);
-        
+
         // Get the user id of the user that create the teacher
         $creatorId = $teacher->user->id;
-        
+
         $this->assertEquals($creatorId, 1);
     }
-    
+
     /***************************************************************/
-    
 }
