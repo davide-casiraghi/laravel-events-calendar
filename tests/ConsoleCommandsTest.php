7397d052c5a773d85b74435d7dd6ee00adc0da19
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-04-01 07:30:41 +0200

    Apply fixes from StyleCI (#180)

--- tests/ConsoleCommandsTest.php
+++ tests/ConsoleCommandsTest.php
@@ -5,7 +5,6 @@ namespace DavideCasiraghi\LaravelEventsCalendar\Tests;
 use DavideCasiraghi\LaravelEventsCalendar\Models\Continent;
 use DavideCasiraghi\LaravelEventsCalendar\Models\Country;
 use DavideCasiraghi\LaravelEventsCalendar\Models\EventVenue;
-
 use Illuminate\Foundation\Testing\WithFaker;
 
 class ConsoleCommandsTest extends TestCase
@@ -13,12 +12,12 @@ class ConsoleCommandsTest extends TestCase
     use WithFaker;
 
     /***************************************************************/
-    
+
     /** @test */
     public function it_execute_retrieve_gps_coordinates_command()
     {
         $this->authenticateAsAdmin();
-        
+
         // Create a teacher by the administrator, that has id 1
         $attributes = factory(EventVenue::class)->raw([
             'continent_id' => 1,
@@ -38,20 +37,18 @@ class ConsoleCommandsTest extends TestCase
             'continent_id' => 1,
         ]);
         $response = $this->post('/eventVenues', $attributes);
-        
+
         $aa = $this->artisan('retrieve-all-gps-coordinates')
-         
-         
-         // @ TODO: still to figure out how to check the command output 
+
+         // @ TODO: still to figure out how to check the command output
          //->expectsQuestion('What is your name?', 'Taylor Otwell')
          //->expectsQuestion('Which language do you program in?', 'PHP')
          //->expectsOutput('52.48789')
          //dd($this->output());
-         
+
          //check if the command run succesfully
          ->assertExitCode(0);
     }
-    
+
     /***************************************************************/
-    
 }
