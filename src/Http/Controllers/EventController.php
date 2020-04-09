88319e45f78c56a779f63a80dd0a9a5b9e20bbeb
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-04-09 05:07:28 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/EventController.php
+++ src/Http/Controllers/EventController.php
@@ -569,11 +569,11 @@ class EventController extends Controller
     public function eventBySlug(string $slug)
     {
         $event = Event::where('slug', $slug)->first();
-        
-        if(is_null($event)){
+
+        if (is_null($event)) {
             abort(404);
         }
-        
+
         $firstRpDates = EventRepetition::getFirstEventRpDatesByEventId($event->id);
 
         return $this->show($event, $firstRpDates);
